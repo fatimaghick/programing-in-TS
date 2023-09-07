28c2d8f7dd3b594770e412796e180d2c22d7e430
fatimaghick <fatimaghick@gmail.com>
2023-09-07 12:24:13 +0500

    changes

---
+++ fatima.js
@@ -0,0 +1,181 @@
+<<<<<<< HEAD
+/*number : var a = 5;
+number : var i =0;
+for (i=1; i<=10; i++){
+    console.log (a + " x " + i + " = " + (a*i));
+}*/
+number: var x = 10;
+do {
+    number: var a = 12;
+    number: var b = 20;
+    number: var c = a + b;
+    console.log(+c);
+    x++;
+} while (x <= 20);
+=======
+/*//Write a program that calculates the area of a rectangle.
+number: var lenght = 20;
+number: var wirth = 30;
+number: var area = lenght*wirth;
+console.log(area);
+
+//Write a program that takes input and calculates the volume of a cube.
+number: var lenght = 20;
+number: var wirth = 30;
+number: var height =70;
+number: var volume = lenght*wirth*height;
+console.log(volume);
+
+//Write a program that checks if a given number is positive, negative, or zero.
+number : var a =90;
+if(a>0){
+console.log("number is positive")
+}
+else {
+console.log("number is negative")
+}
+number : var a =-90;
+if(a<0){
+console.log("number is negative")
+}
+else {
+console.log("number is positive")
+}
+number : var a =0;
+if(a==0){
+console.log("number is zero")
+}
+else {
+console.log("number is positive")
+}
+
+//Write a program that checks if a given number is even or odd.
+number : var a =18;
+number : var b =(a%2);
+if(b==0){
+console.log("number is even")
+}
+else {
+console.log("number is odd")
+}
+number : var a =5;
+number : var b =(a%2);
+if(b!=0){
+console.log("number is odd")
+}
+else {
+console.log("number is even")
+}
+// Write a program that calculates the result of a mathematical expression.
+((10 + 5) * 3 - 2)/(4%3)-7
+ 
+//Write a program that determines if a person is eligible to vote based on their age.
+
+number : var a =18;
+if(a<0){
+console.log("person is eligible to vote")
+}
+else {
+console.log("person is not  eligible to vote")
+}
+
+number : var a = 5;
+number : var i =0;
+for (i=1; i<=10; i++){
+    console.log (a + " x " + i + " = " + (a*i));
+}
+
+number : var x = 10;
+
+do {
+number : var a =12;
+number : var b =20;
+number : var c =a+b;
+console.log (+c);
+x++;
+}while(x<=20);*/
+/*var carshonda =[{
+    company : "toyota",
+    isUsedCar : "false",
+    year : 2023,
+} , {
+    company : "honda",
+    isUsedCar : "true",
+     year : 2024,
+}
+ 
+]
+  console.log(carshonda[0].year)*/
+/* type Girl = {
+   name: string;
+   age: number;
+   height: number;
+   education: string;
+ };
+ 
+ var students: Girl[] = [
+   { name: 'Fatima', age: 18, height: 5, education: 'FSC' },
+   { name: 'Aysha', age: 18, height: 5, education: 'Matric' },
+   { name: 'Zainab', age: 18, height: 5, education: 'Grade 7' },
+   { name: 'Tayyaba', age: 18, height: 5, education: 'Grade 4' }
+ ];
+ 
+ console.log(students[0].name); */
+/*type chair = {
+  company: string,
+  material: string,
+  price: number,
+  color: string,
+  isUsedChair: boolean
+};
+
+var furniture: chair[] = [
+  { company: "Master", material: "wood", price: 500, color: "brown", isUsedChair: true },
+  { company: "Delux", material: "Plastic", price: 600, color: "black", isUsedChair: false }
+];
+
+console.log(furniture[0].company);
+
+
+//Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
+
+function CtoFTemperature () {
+  number: var temp = 45;
+  number : var tempf = temp * (9/5) +32 ;
+  console.log("tempf:", tempf)
+}
+CtoFTemperature ()
+
+//Write a program that calculates the percentage.
+function calculatepercentage () {
+  number : var obtainedmarks = 1070;
+  number : var totalmarks = 1100;
+  number : var percentage = obtainedmarks / totalmarks * 100;
+  console.log ("percentage:", percentage);
+}
+
+calculatepercentage ()*/
+// Create a program that determines the category of a user-provided age. If the age is between 0 and 12, print "Child." If it's between 13 and 19, print "Teenager." Otherwise, print "Adult."
+number: var age = 5;
+if (age <= 12) {
+    console.log("child");
+}
+else {
+    console.log("teenage");
+}
+number: var age = 15;
+if (age <= 19) {
+    console.log("teenage");
+}
+else {
+    console.log("child");
+}
+number: var age = 15;
+if (age >= 19) {
+    console.log("teenage");
+}
+else {
+    console.log("adult");
+}
+//Write a program that converts given number of days in to weeks and days such as 17 days = 2 weeks and 3 days.
+>>>>>>> afe6fcc27c9a2bc1dc5b665d0de908593e9de581

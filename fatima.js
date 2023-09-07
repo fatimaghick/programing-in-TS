b3f0580ad041b55652645bd2255c23a57d6b2556
fatimaghick <fatimaghick@gmail.com>
2023-09-07 10:57:38 +0500

    first commit

---
+++ fatima.js
@@ -0,0 +1,13 @@
+/*number : var a = 5;
+number : var i =0;
+for (i=1; i<=10; i++){
+    console.log (a + " x " + i + " = " + (a*i));
+}*/
+number: var x = 10;
+do {
+    number: var a = 12;
+    number: var b = 20;
+    number: var c = a + b;
+    console.log(+c);
+    x++;
+} while (x <= 20);
